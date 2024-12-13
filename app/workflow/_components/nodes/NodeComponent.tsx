485f16dae45d7b78c02e7bf86f2570437d38584d
Aditya <adityasinghboss1234@gmail.com>
2024-12-14 04:39:27 +0530

    Node Inputs
    - Added Node Inputs based on different input type (different params)

--- app/workflow/_components/nodes/NodeComponent.tsx
+++ app/workflow/_components/nodes/NodeComponent.tsx
@@ -1,16 +1,24 @@
+import { TaskRegistry } from "@/lib/workflow/task/registry";
+import { AppNodeData } from "@/types/appNode";
 import { NodeProps } from "@xyflow/react";
 import { memo } from "react";
 import NodeCard from "./NodeCard";
 import NodeHeader from "./NodeHeader";
-import { AppNodeData } from "@/types/appNode";
+import { NodeInput, NodeInputs } from "./NodeInputs";
 
 const NodeComponent = memo((props: NodeProps) => {
     const nodeData = props.data as AppNodeData;
+    const task = TaskRegistry[nodeData.type];
 
     return (
         <NodeCard nodeId={props.id} isSelected={!!props.selected}>
             {/* The !! syntax converts the value of props.selected to a boolean */}
             <NodeHeader taskType={nodeData.type} />
+            <NodeInputs>
+                {task.inputs.map((input) => (
+                    <NodeInput key={input.name} input={input} />
+                ))}
+            </NodeInputs>
         </NodeCard>
     );
 });

8b75a8f8c6c432caf13ea1fd4dc43b686a5ef47f
Aditya <adityasinghboss1234@gmail.com>
2024-12-14 05:10:43 +0530

    Node Param Field
    - String Param

--- app/workflow/_components/nodes/NodeParamField.tsx
+++ app/workflow/_components/nodes/NodeParamField.tsx
@@ -2,11 +2,43 @@
 
 import { TaskParam, TaskParamType } from "@/types/task";
 import StringParam from "./param/StringParam";
+import { useReactFlow } from "@xyflow/react";
+import { AppNode } from "@/types/appNode";
+import { useCallback } from "react";
 
-function NodeParamField({ param }: { param: TaskParam }) {
+function NodeParamField({
+    param,
+    nodeId,
+}: {
+    param: TaskParam;
+    nodeId: string;
+}) {
+    const { updateNodeData, getNode } = useReactFlow();
+    const node = getNode(nodeId) as AppNode;
+    const value = node?.data.inputs?.[param.name];
+
+    console.log("@Value", value);
+
+    const updateNodeParamValue = useCallback(
+        (newValue: string) => {
+            updateNodeData(nodeId, {
+                inputs: {
+                    ...node.data.inputs,
+                    [param.name]: newValue,
+                },
+            });
+        },
+        [updateNodeData, nodeId, node?.data.inputs, param.name]
+    );
     switch (param.type) {
         case TaskParamType.STRING:
-            return <StringParam param={param} />;
+            return (
+                <StringParam
+                    param={param}
+                    value={value}
+                    updateNodeParamValue={updateNodeParamValue}
+                />
+            );
         default:
             return (
                 <div className="w-full">
--- app/workflow/_components/nodes/param/StringParam.tsx
+++ app/workflow/_components/nodes/param/StringParam.tsx
@@ -2,14 +2,12 @@
 
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { TaskParam } from "@/types/task";
-import { useId } from "react";
+import { ParamProps } from "@/types/appNode";
+import { useId, useState } from "react";
 
-interface ParamProps {
-    param: TaskParam;
-}
+function StringParam({ param, value, updateNodeParamValue }: ParamProps) {
+    const [internalValue, setInternalValue] = useState(value);
 
-function StringParam({ param }: ParamProps) {
     const id = useId();
     return (
         <div className="space-y-1 p-1 w-full">
@@ -17,7 +15,14 @@ function StringParam({ param }: ParamProps) {
                 {param.name}
                 {param.required && <p className="text-red-400 px-2">*</p>}
             </Label>
-            <Input id={id} />
+            <Input
+                id={id}
+                className="text-xs"
+                value={internalValue}
+                placeholder="Enter value here"
+                onChange={(e) => setInternalValue(e.target.value)}
+                onBlur={(e) => updateNodeParamValue(e.target.value)}
+            />
             {param.helperText && (
                 <p className="text-muted-foreground px-2">{param.helperText}</p>
             )}

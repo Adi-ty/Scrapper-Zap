7b60cbed6a00484bf17b933d5493344e615622e4
Aditya Singh <adityasinghboss1234@gmail.com>
2026-01-17 08:39:49 +0530

    Add Click Element task and executor with UI integration

--- lib/workflow/executor/ClickElementExecutor.ts
+++ lib/workflow/executor/ClickElementExecutor.ts
@@ -1,21 +1,16 @@
 import { ExecutionEnvironment } from "@/types/executor";
-import { FillInputTask } from "../task/FillInput";
+import { ClickElementTask } from "../task/ClickElement";
 
-export async function FillInputExecutor(
-    environment: ExecutionEnvironment<typeof FillInputTask>,
+export async function ClickElementExecutor(
+    environment: ExecutionEnvironment<typeof ClickElementTask>,
 ): Promise<boolean> {
     try {
         const selector = environment.getInput("Selector");
         if (!selector) {
             environment.log.error("No selector provided.");
         }
-        const value = environment.getInput("Value");
-        if (!value) {
-            environment.log.error("No value provided.");
-        }
 
-        await environment.getPage()!.type(selector, value);
-
+        await environment.getPage()!.click(selector);
         return true;
     } catch (error: any) {
         environment.log.error(error.message);
--- lib/workflow/task/registry.tsx
+++ lib/workflow/task/registry.tsx
@@ -4,6 +4,7 @@ import { ExtractTextFromElementTask } from "@/lib/workflow/task/ExtractTextFromE
 import { TaskType } from "@/types/task";
 import { WorkflowTask } from "@/types/workflow";
 import { FillInputTask } from "./FillInput";
+import { ClickElementTask } from "./ClickElement";
 
 type Registry = {
     [k in TaskType]: WorkflowTask & { type: k }; // Ensure that the type field in the task object is the same as the key in the registry
@@ -14,4 +15,5 @@ export const TaskRegistry: Registry = {
     PAGE_TO_HTML: PageToHtmlTask,
     EXTRACT_TEXT_FROM_ELEMENT: ExtractTextFromElementTask,
     FILL_INPUT: FillInputTask,
+    CLICK_ELEMENT: ClickElementTask,
 };
